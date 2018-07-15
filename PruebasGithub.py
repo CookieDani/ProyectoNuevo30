9dfb65c936e8f9c45386a8feb2c0c0b68489736f
CookieDanie <soyyo.peralta@gmail.com>
2018-07-15 00:38:49 -0500

    Cambios

--- PruebasGithub.py
+++ PruebasGithub.py
@@ -1,2 +1,12 @@
+def fibbonacci(n):
+    sum = 1
+    prim = 0
+    for i in range(n+1):
+        print(prim)
+        aux = sum
+        sum += prim
+        prim = aux
+
+
 if __name__ == '__main__':
-    print("Hola mundo")
+    fibbonacci(int(input("Ingrese un valor: ")))
